41eccfaeb655dc43fb60479f56dd42373be4ab54
rishabhAjay <rishabhajay24@gmail.com>
2022-09-18 13:45:21 +0530

    eighth commit

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { exec } from "child_process";
 dotenv.config();
 export const PORT = process.env.PORT || 4000;
 
@@ -7,5 +8,18 @@ export const USER = process.env.USER;
 export const PASSWORD = process.env.PASSWORD;
 export const DB = process.env.DB;
 export const DSN = process.env.DSN;
-export const VERSION = process.env.VERSION;
+let VERSION;
+exec("npx sentry-cli releases propose-version", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  VERSION = stdout;
+});
+export { VERSION };
 export const dialect = "mysql";
