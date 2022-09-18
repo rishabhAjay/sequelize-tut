8d34ad027d5d4a5ae41c3097c0764caf320ff7a1
rishabhAjay <rishabhajay24@gmail.com>
2022-09-18 16:51:35 +0530

    commit 23

--- sentry.sh
+++ sentry.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
+if [ "$1" == "" ]; then
+    echo "No remote alias provided"
+    exit 1
+fi
 
+if [ "$2" == "" ]; then
+    echo "No branch provided"
+    exit 1
+fi
+
+git push $1 $2
 VERSION=`npx sentry-cli releases propose-version`
 # Workflow to create releases
 npx sentry-cli releases  --org student-qlx --project node-express new "$VERSION"
